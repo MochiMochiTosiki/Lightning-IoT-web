9f68228aba8c22df4c8a877c767cc402a6067801
toshiki <mochi.mochi.037@gmail.com>
2019-02-13 02:30:56 +0900

    My Lightning Web Site

--- lightningweb/settings.py
+++ lightningweb/settings.py
@@ -139,7 +139,7 @@ except ImportError:
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = OS.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
